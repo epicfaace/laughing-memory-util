bf759030256a4763a02a3e48ab577cd082e7388b
Ashwin Ramaswami <aramaswamis@gmail.com>
2018-03-09 15:32:02 -0800

    fix typo

--- laughing_memory_util/data_aug.py
+++ laughing_memory_util/data_aug.py
@@ -111,4 +111,4 @@ def data_aug(TRAIN_PATH, TEST_PATH, IMG_WIDTH, IMG_HEIGHT, IMG_CHANNELS, run_tim
         break
 
     print('\n Training images succesfully downsampled!')
-    return np.concatenate((X_train_aug, X_train)), axis=0), np.concatenate((Y_train_aug, Y_train), axis=0)
+    return np.concatenate((X_train_aug, X_train), axis=0), np.concatenate((Y_train_aug, Y_train), axis=0)
