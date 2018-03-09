fb7b08ba3743d658a8c698a476f6696b0bdd1a4f
Ashwin Ramaswami <aramaswamis@gmail.com>
2018-03-09 12:25:26 -0800

    name

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='Laughing memory util',
+setup(name='laughing_memory_util',
       version='0.1',
       description='The funniest joke in the world',
       url='https://github.com/epicfaace/laughing-memory-util',
